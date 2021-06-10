34da63526df4a3e355de368bb6a560737c43950e
Ankur Ankan <ankurankan@gmail.com>
2021-06-10 11:01:57 +0200

    Final

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def compute_next_state(state):
     new_state = np.zeros(state.shape, dtype=int)
     for i in range(state.shape[0]):
         for j in range(state.shape[1]):
-            low_x, high_x = max(0, i-1), min(i+1, state.shape[0])
-            low_y, high_y = max(0, j-1), min(j+1, state.shape[0])
+            low_x, high_x = max(0, i-1), min(i+2, state.shape[0])
+            low_y, high_y = max(0, j-1), min(j+2, state.shape[1])
             n_live = np.sum(state[low_x: high_x, low_y: high_y]) - state[i, j]
 
             if (state[i, j] == 1) and (n_live < 2):
@@ -23,18 +23,18 @@ def compute_next_state(state):
                 new_state[i, j] = 0
             elif (state[i, j] == 0) and (n_live == 3):
                 new_state[i, j] = 1
-                print("i am here")
             else:
                 new_state[i, j] = state[i, j]
 
     return new_state
 
 
-def start(initial_state=None, loop_delay=1, size=(100, 100)):
+def start(initial_state=None, loop_delay=1, size=(800, 800)):
     if initial_state is None:
         state = get_initial_state(size)
     else:
-        state = intial_state
+        state = initial_state
+        size = state.shape
 
     age = np.zeros(size, dtype=int)
     counter = 0
@@ -44,7 +44,9 @@ def start(initial_state=None, loop_delay=1, size=(100, 100)):
         age += state
         age = age * state
         counter += 1
-        plt.imshow(state.repeat(4, axis=0).repeat(4, axis=1))
+        plt.imshow(state)
+        plt.xlim(right=size[1], left=0)
+        plt.ylim(top=0, bottom=size[0])
         plt.pause(loop_delay)
 
         if np.sum(state) == 0:
